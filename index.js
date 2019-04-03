983825bfcf434853b72eecad2076340ffaa33dd5
emailboxsachin <emailbox.sachin@rediffmail.com>
2019-04-03 23:05:30 +0530

    added new files for routes

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser');
 const hbs = require('express-handlebars')
+const jobsRoute = require('./routes/jobs')
 var mongoose = require('mongoose');
 const app = express()
 var db = mongoose.connect('mongodb://localhost/nodeman');
+const port = 3001
 
 app.engine('hbs', hbs({extname:'hbs', defaultLayout:'layout',layoutDir:__dirname + '/views/layouts/'}))
 app.set('views', path.join(__dirname, 'views'))
@@ -14,17 +16,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
 
 
-app.get('/', (req,res,next)=>{
-	res.render('index')
-})
+app.get('/', jobsRoute.getAllJobs)
 
 
-
-app.use('/projectSupport',  require('./routes/project-support'))
-
-app.use('/projectholder',  require('./routes/project-holder'))
-
-app.use('/project',  require('./routes/project'))
-
-app.listen(3000)
-console.log('Server running on http://localhost:3000')
+app.listen(port)
+console.log('Server running on http://localhost:'+port)
---
+++ routes/jobs.js
@@ -0,0 +1,44 @@
+var model= require('../model/jobs')
+
+module.exports.getAllJobs = function(req,res,next){
+	// model.find({}).sort({timeStamp: 'desc'}).select('_id name email address1').exec((err,ph)=>{
+	model.find({}).sort({timeStamp: 'desc'}).exec((err,data)=>{	
+		if(err){
+			res.send('error occured' + JSON.stringify(err))
+		}else{
+			res.render('index',{title:'Node Man', jobs:data})	
+		}
+	})
+}
+
+// router.get('/',async (req,res,next)=>{
+// 	if(req.query.psid){
+// 		ph.find({projectSupportId:req.query.psid}).sort({timeStamp: 'desc'}).select('_id name email address1').exec((err,ph)=>{
+// 			if(err){
+// 				res.send('error occured' + JSON.stringify(err))
+// 			}else{
+// 				res.render('project-holder',{title:'project Holder', psid:req.query.psid, projectholder:ph})	
+// 			}
+// 		})		
+// 	}
+// })
+
+// router.post('/new',(req,res,next)=>{
+// 	const body = req.body
+// 	body.user = req.session.user
+// 	if(body !== null){
+// 		//save in db
+// 		console.log(JSON.stringify(body))
+// 		ph.create(body,function(err,projholder){
+// 			if(err){
+// 				res.send('error')
+// 			}else{
+// 				console.log('inserted data'+projholder)
+// 				res.redirect('/projectholder?psid='+body.projectSupportId)
+// 			}
+// 		})
+// 	}
+// })
+
+
+
